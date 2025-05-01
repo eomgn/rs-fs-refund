43740c9034b46544476b223705794961f943e330
eomgn <matheeusgn21@gmail.com>
2025-05-01 17:38:27 -0300

    input amount somente com numeros - removendo letras

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-console.log("JavaScript connected!!!");
+// selecionando elementos do formulário
+const amountInput = document.querySelector("#amount");
+
+// amountInput.oninput = () => {
+//   console.log("conteudo mudou!!");
+// };
+
+// verificando quando o conteudo do input mudou
+amountInput.addEventListener("change", () => {
+  let value = amountInput.value.replace(/\D/g, "");
+
+  amountInput.value = value;
+});

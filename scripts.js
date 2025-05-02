2c6e1a0f38a03e38aa67d48c0dbbed0bc0580c0b
eomgn <matheeusgn21@gmail.com>
2025-05-02 17:59:44 -0300

    formatando como moeda

--- scripts.js
+++ scripts.js
@@ -1,13 +1,30 @@
 // selecionando elementos do formulário
 const amountInput = document.querySelector("#amount");
 
+// verificando quando o conteudo do input mudou
+amountInput.addEventListener("change", () => {
+  // obtem o valor atual do input e remove os caracteres nao numericos
+  let value = amountInput.value.replace(/\D/g, "");
+
+  // transformando o valor em centavos (ex: 150/100 = 1.5 que é equivalente a R$ 1,50)
+  value = Number(value) / 100;
+
+  // atualiza o valor do input
+  amountInput.value = formatCurrencyBRL(value);
+});
+
 // amountInput.oninput = () => {
 //   console.log("conteudo mudou!!");
 // };
 
-// verificando quando o conteudo do input mudou
-amountInput.addEventListener("change", () => {
-  let value = amountInput.value.replace(/\D/g, "");
+// formatando como moeda
+function formatCurrencyBRL(value) {
+  // formatando no padrão BRL (R$)
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
 
-  amountInput.value = value;
-});
+  // retorna o valor formatado
+  return value;
+}

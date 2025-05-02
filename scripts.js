d0642950f3d6a961d1c2a5131fd6edecb892958b
eomgn <matheeusgn21@gmail.com>
2025-05-02 20:03:35 -0300

    funcao de um novo item na lista

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@ const amountInput = document.querySelector("#amount");
 const expenseInput = document.querySelector("#expense");
 const categoryInput = document.querySelector("#category");
 
+// selecionando elementos da lista
+const expenseList = document.querySelector("ul");
+const expensesQuantity = document.querySelector("aside header p span");
+
 // verificando quando o conteudo do input mudou
 amountInput.addEventListener("change", () => {
   // obtem o valor atual do input e remove os caracteres nao numericos
@@ -15,10 +19,6 @@ amountInput.addEventListener("change", () => {
   // atualiza o valor do input
   amountInput.value = formatCurrencyBRL(value);
 });
-
-// amountInput.oninput = () => {
-//   console.log("conteudo mudou!!");
-// };
 
 // formatando como moeda
 function formatCurrencyBRL(value) {
@@ -32,11 +32,12 @@ function formatCurrencyBRL(value) {
   return value;
 }
 
-// form.onsubmit;
-
+// captura o evento de submit do formulario para obter os valores
 form.addEventListener("submit", (event) => {
+  // previre o comportamento padrão de recarregar a página
   event.preventDefault();
 
+  // cria um objeto com os detalhes da nova despesa
   const newExpense = {
     id: new Date().getTime(),
     expense: expenseInput.value,
@@ -46,5 +47,69 @@ form.addEventListener("submit", (event) => {
     created_at: new Date(),
   };
 
-  console.log(newExpense);
+  // console.log(newExpense);
+
+  // chama a funcao que ira adicionar o item na lista
+  expenseAdd(newExpense);
 });
+
+//adiciona um novo item na lista
+function expenseAdd(newExpense) {
+  try {
+    // cria o elemento li para adicionar o item (li) na lista (ul)
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    // ---
+    // cria o icone da categoria
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    // ---
+    // cria a info da despesa
+    const expenseInfo = document.createElement("div");
+    expenseInfo.classList.add("expense-info");
+
+    // cria o nome da despesa
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = newExpense.expense;
+
+    // cria a categoria da despesa
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = newExpense.category_name;
+
+    // adicionar nome e categoria na div das informações da despesa
+    expenseInfo.append(expenseName, expenseCategory);
+
+    // ---
+    // cria o valor da despesa
+    const expenseAmount = document.createElement("span");
+    expenseAmount.classList.add("expense-amount");
+    expenseAmount.innerHTML = `
+      <small>R$</small>
+      ${newExpense.amount.toUpperCase().replace("R$", "")}
+      `;
+
+    // ---
+    // cria o icone de remover
+    const removeIcon = document.createElement("img");
+    removeIcon.setAttribute("src", "/img/remove.svg");
+    removeIcon.setAttribute("alt", "remover");
+    removeIcon.classList.add("remove-icon");
+
+    // ---
+    // adiciona as informações no item
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
+
+    // ---
+    // adiciona o item na lista
+    expenseList.append(expenseItem);
+
+    // atualiza os totais
+    updateTotals();
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas.");
+    console.log(error);
+  }
+}
